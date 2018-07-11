307cbff970ce70bb990f3def459c78dbc119f395
David <davidgcastro93@gmail.com>
2018-07-11 11:20:01 -0400

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,21 +1,2 @@
-const app = require('express')();
-//Allows express to understand graphql and allows express server to have a simple way to use its api.
-const graphqlHTTP = require('express-graphql');
-//get schema
-const schema = require('./schema/schema');
-
-//use it as middleware! an enpoint to interact with our graphql data.
-//pass schema, tells graphql about our data and how data will look. What data type, relationships, etc
-//ultimate goal is to have request jump into any point in graphql map
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    //schema we created in schema.js
-    schema,
-    //use the tool!
-    graphiql: true
-  })
-);
-app.listen(3000, function() {
-  console.log('Listening on port 3000!');
-});
+const express = require('express');
+const morgan = require('morgan');
