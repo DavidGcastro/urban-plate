4cf3373f52c85fcd5aa61aedd5c903eef9ab8d48
David <davidgcastro93@gmail.com>
2018-07-14 23:40:12 -0400

    finished title and nav

--- client/components/Title.js
+++ client/components/Title.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const Title = () => {
-  return <h1> lol </h1>;
+  return (
+    <div className="title-container">
+      <div className="title-innerContainer">
+        <img
+          src="https://d33wubrfki0l68.cloudfront.net/d572e4e96f680e3f07482265cc0e3900cebc9056/3cc56/images/logo_urbanplate-d572e4e9.svg"
+          width="150"
+          className="title-logo"
+          alt="Logo urbanplate"
+        />
+        <h2 className="title-about">
+          Modern, delicious and creative vego food.<br />
+          This isn’t your usual vegetarian site.
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Title;
